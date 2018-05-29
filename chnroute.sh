9440bc926017bf2409e5323a1cc2e383ca8fe183
Ricky-Hao <a471558277@gmail.com>
2018-05-30 00:07:25 +0800

    Rename chnroute.rules.

--- chnroute.sh
+++ chnroute.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-echo "=CHNRoute,仅国内直连,1,1,1,0,0,2," > CHNRoute.rules
+echo "=CHNRoute,不代理国内IP,1,1,1,0,0,2," > CHNRoute.rules
 curl -s http://www.ipdeny.com/ipblocks/data/aggregated/cn-aggregated.zone >> CHNRoute.rules
 git add CHNRoute.rules
 git commit -m "CHNRoute.rules auto update. `date +"%Y-%m-%d"`"
