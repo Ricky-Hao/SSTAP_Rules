381216a1fd370e70aa11c4b5b2b6ba0975df743d
Ricky-Hao <a471558277@gmail.com>
2018-06-01 17:28:53 +0800

    CHNRoute.rules auto update. 2018-06-01

--- chnroute.sh
+++ chnroute.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-echo "=CHNRoute,CHNRoute,1,1,1,0,0,2," > CHNRoute.rules
+echo "=CHNRoute,CHNRoute,1,1,1,0,0,2,`date +"%Y-%m-%d"`" > CHNRoute.rules
 curl -s http://www.ipdeny.com/ipblocks/data/aggregated/cn-aggregated.zone >> CHNRoute.rules
 git add CHNRoute.rules
 git commit -m "CHNRoute.rules auto update. `date +"%Y-%m-%d"`"
