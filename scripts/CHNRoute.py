3f12e4fa50ebd763654e48f84092f200da1cb261
Ricky-Hao <a471558277@gmail.com>
2018-06-06 19:41:59 +0800

    Add my vps ip to CHNRoute.

--- scripts/CHNRoute.py
+++ scripts/CHNRoute.py
@@ -5,9 +5,10 @@ import os
 scripts_path = os.path.abspath(__file__)
 rules_path = os.path.join(os.path.dirname(os.path.dirname(scripts_path)), 'rules')
 file_path = os.path.join(rules_path, 'CHNRoute.rules')
+vps_ip = '47.75.42.144/32\n'
 
 
 header = "=CHNRoute,CHNRoute,1,1,1,0,0,2,{0}\n".format(time.strftime('%Y-%m-%d'))
 content = requests.get('http://www.ipdeny.com/ipblocks/data/aggregated/cn-aggregated.zone').text
 with open(file_path, 'w+') as f:
-    f.write(header+content)
+    f.write(header+content+vps_ip)
