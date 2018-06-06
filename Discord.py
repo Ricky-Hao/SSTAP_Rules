41e89ad4fb0a7a1c73350d16e6f2fa31462e4bf2
Ricky-Hao <a471558277@gmail.com>
2018-06-06 18:04:37 +0800

    Rules auto update. 2018-06-06

---
+++ Discord.py
@@ -0,0 +1,23 @@
+import socket
+import requests
+import time
+
+header = '=Discord,Discord,0,0,1,0,1,2,{0}\n'.format(time.strftime('%Y-%m-%d'))
+url = '{0}{1}.discord.gg'
+ip_list = []
+local_ip = requests.get('http://ipv4.icanhazip.com').text.strip()
+
+for area in ['hongkong', 'japan']:
+    for i in range(999):
+        try:
+            ip = socket.gethostbyname(url.format(area, i))
+            if ip != local_ip:
+                ip_list.append(ip)
+        except socket.gaierror as err:
+            pass
+
+with open('Discord.rules', 'w+') as f:
+    f.write(header+"/32\n".join(ip_list)+'/32')
+
+
+        
