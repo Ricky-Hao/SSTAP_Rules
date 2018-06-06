03bec50ae6666b195e247a0dd8794601cdafe3a3
Ricky-Hao <a471558277@gmail.com>
2018-06-06 18:06:04 +0800

    Remove useless files.

--- chnroute.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-echo "=CHNRoute,CHNRoute,1,1,1,0,0,2,`date +"%Y-%m-%d"`" > CHNRoute.rules
-curl -s http://www.ipdeny.com/ipblocks/data/aggregated/cn-aggregated.zone >> CHNRoute.rules
-git add CHNRoute.rules
-git commit -m "CHNRoute.rules auto update. `date +"%Y-%m-%d"`"
-git push
